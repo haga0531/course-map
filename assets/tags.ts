e9e8457062eadcae985c1835b0585e073bc757f9
hagahiroki <bagy531hiro@gmail.com>
2020-07-18 13:20:02 +0900

    デザイン->ui/ux

---
+++ assets/tags.ts
@@ -0,0 +1,28 @@
+const tags = [
+  'Javascript/TypeScript',
+  'コンテナ',
+  'クラウド',
+  'コンピュータサイエンス',
+  'HTML/CSS',
+  'Ruby/Rails',
+  'PHP/Laravel',
+  'Python',
+  'C/C++',
+  'C#',
+  'Go',
+  'Swift',
+  'Kotlin',
+  'Java',
+  'Git',
+  'SQL',
+  'Vue.js',
+  'React.js',
+  'UNIX (Linux)',
+  'ネットワーク',
+  'NoCode',
+  'UI/UX',
+  'VBA',
+  'その他'
+]
+
+export default tags
